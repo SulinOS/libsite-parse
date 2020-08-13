7c7a89c2bf0f2c62f38ddcf8a09743dd4f62d538
Your Name <you@example.com>
2020-08-13 12:50:38 +0000

    temizlik

--- link.c
+++
@@ -1,7 +0,0 @@
-#include <header.h>
-char* getLink(char *src,char *search){
-	char*tmp=strgrep(src,search);
-	tmp=strgrep(tmp,"<a");
-	tmp=getToken(tmp,"href");
-	return tmp;
-}
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <header.h>
-#include <string.h>
-int main(int argc,char *argv[]){
-	char*site=read_site("https://www.gnu.org/");
-	printf("%s",getLink(site,".html"));
-}
-
